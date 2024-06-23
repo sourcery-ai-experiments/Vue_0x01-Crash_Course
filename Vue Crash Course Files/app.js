cc09110ab3630e5de96940ffe5df6f47cd12e32d
Joseph Bamisaye <joebamisaye068@gmail.com>
2024-06-23 21:41:34 +0100

    feat: learn directives

--- Vue Crash Course Files/app.js
+++ Vue Crash Course Files/app.js
@@ -1,8 +1,8 @@
 const vm = Vue.createApp({  // vm stands for vue module. The word module describe the data we have in our app. vue describe the html we have in our app.
     data() {
       return {
-         firstName: 'Dave',
-         lastName: 'Joe'
+        firstName: 'Dave',
+        lastName: 'Joe'
       }
     },
     methods: {
